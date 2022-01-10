205b7c2ee8979e473fddbcb7716543a27452705b
Nicolas <pnwa@icloud.com>
2022-01-10 23:48:47 +0100

    Add User model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+// User Schema
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    trim: true,
+    default: "reader",
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    trim: true,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+})
+
+// Hash password
+userSchema.statics.hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10)
+  return await bcrypt.hash(password, salt)
+}
+
+// Compare password and hashed password
+userSchema.statics.comparePassword = async (receivedPassword, hashedPassword) => {
+  return await bcrypt.compare(receivedPassword, hashedPassword)
+}
+
+// Export User Model
+module.exports = mongoose.model("User", userSchema)
