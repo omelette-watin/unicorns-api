f02b00aaf39bc8c53cc35992e64473238059359e
Nabil <nabil.ainas@supdevinci-edu.fr>
2022-01-25 19:28:08 +0100

    Add favs methods

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,5 +1,6 @@
 const Post = require("../models/Post")
 const Comment = require("../models/Comment")
+const User = require("../models/User")
 const { now } = require("mongoose")
 
 
@@ -10,33 +11,6 @@ exports.countAllPublishedPosts = async (req, res) => {
 
     return res.status(200).json({
       count
-    })
-
-  } catch (e) {
-
-    return res.status(500).json({
-      message: e.message || "Oups il y a eu une erreur veuillez réessayer plus tard",
-    })
-
-  }
-}
-
-exports.getRandomPost = async (req, res) => {
-  try {
-    const ids = await Post.find({ isPublished: true }).select({ _id: 1 })
-
-    const randomIndex = Math.floor(Math.random() * (ids.length))
-
-    const randomId = ids[randomIndex]._id
-
-    const randomPost = await Post.findOne({ _id: randomId, isPublished: true })
-
-    if (!randomPost) return res.status(404).json({
-      message: "Cet article n'existe pas"
-    })
-
-    return res.status(200).json({
-      randomPost
     })
 
   } catch (e) {
@@ -90,6 +64,59 @@ exports.getAllPublishedPosts = async (req, res) => {
       isPublished: true,
       title: { "$regex": `${search}`, "$options" : "i" },
       category: { "$regex": `${category}`, "$options": "i" }
+    })
+
+    return res.status(200).json({
+      meta: {
+        totalCount,
+        pageCount: Math.ceil(totalCount / limit),
+        currentPage: page,
+        perPage: limit
+      },
+      result: posts
+    })
+
+  } catch (e) {
+
+    return res.status(500).json({
+      message: e.message || "Oups il y a eu une erreur veuillez réessayer plus tard",
+    })
+
+  }
+}
+
+exports.getFavPosts = async (req, res) => {
+  const page = parseInt(req.query.page) || 1
+  const limit = parseInt(req.query.limit) || 5
+  const skipIndex = (page - 1) * limit
+  const order = req.query.order || "latest"
+  const search = req.query.q || ""
+  const category = req.query.category || ""
+
+  try {
+
+    const userId = req.userId
+
+    const user = await User.findById(userId)
+
+    const userFavs = user.favs
+
+    const posts = await Post.find(
+      {
+        isPublished: true,
+        title: { "$regex": `${search}`, "$options": "i" },
+        category: { "$regex": `${category}`, "$options": "i" },
+        _id: { $in: userFavs }
+      })
+      .sort({ createdAt: (order === "latest") ? -1 : 1 })
+      .limit(limit)
+      .skip(skipIndex)
+
+    const totalCount = await Post.countDocuments({
+      isPublished: true,
+      title: { "$regex": `${search}`, "$options" : "i" },
+      category: { "$regex": `${category}`, "$options": "i" },
+      _id: { $in: userFavs }
     })
 
     return res.status(200).json({
@@ -255,6 +282,70 @@ exports.getSavedPostById = async (req, res) => {
   }
 }
 
+exports.addPostToFavs = async (req, res) => {
+  const { id } = req.params
+
+  try {
+
+    const post = await Post.findOne({ _id: id, isPublished: true })
+
+    if (!post) return res.status(404).json({message: "Cet article n'existe pas"})
+
+    const userId = req.userId
+
+    const user = await User.findById(userId)
+
+    const userFavs = user.favs.slice()
+
+    userFavs.push(id)
+
+    await User.updateOne({ _id: userId }, { favs: userFavs })
+
+    return res.status(200).json({
+      message: "L'article a bien été ajouté aux favoris",
+    })
+
+  } catch (e) {
+
+    return res.status(500).json({
+      message: e.message || "Oups il y a eu une erreur veuillez réessayer plus tard",
+    })
+
+  }
+
+}
+
+exports.removePostFromFavs = async (req, res) => {
+  const { id } = req.params
+
+  try {
+
+    const post = await Post.findOne({ _id: id, isPublished: true })
+
+    if (!post) return res.status(404).json({message: "Cet article n'existe pas"})
+
+    const userId = req.userId
+
+    const user = await User.findById(userId)
+
+    const userFavs = user.favs.filter( fav => fav !== id )
+
+    await User.updateOne({ _id: userId }, { favs: userFavs })
+
+    return res.status(200).json({
+      message: "L'article a bien été enlevé des favoris",
+    })
+
+  } catch (e) {
+
+    return res.status(500).json({
+      message: e.message || "Oups il y a eu une erreur veuillez réessayer plus tard",
+    })
+
+  }
+
+}
+
 exports.createPost = async (req, res) => {
   const { title, content, category } = req.body
 
@@ -393,3 +484,5 @@ exports.deletePost = async (req, res) => {
 
   }
 }
+
+
